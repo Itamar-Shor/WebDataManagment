b61d28eb358baf1c12531dea42bab066ae4ed262
ItamarS <itamarshor67@gmail.com>
2022-07-02 13:17:51 +0300

    IR - evaluation part

--- Info_retrieval/ir_evaluation.py
+++ Info_retrieval/ir_evaluation.py
@@ -1,13 +1,24 @@
 import os
 from lxml import etree
+from Information_retrieval import InformationRetrieval
+import numpy as np
+
+"""
+query_db = {
+    'query': <query>,
+    'results': {
+        <doc>: <score>,
+    }
+}
+"""
 
 
 def load_query_db(xml_path):
     with open(xml_path, 'r') as fd:
-        query_db = etree.fromstring(fd.read())
+        db = etree.fromstring(fd.read())
 
     query_db = []
-    query_items = query_db.xpath("//QUERY")
+    query_items = db.xpath("//QUERY")
     for query_item in query_items:
         # q_num = query_item.xpath("./QueryNumber/text()")
         q_text = query_item.xpath("./QueryText/text()")[0]
@@ -16,4 +27,48 @@ def load_query_db(xml_path):
         for q_res in q_results:
             doc = q_res.xpath('./text()')[0]
             score = q_res.xpath('./@score')[0]
-            query_db[-1]['results']['doc'] = score
+            query_db[-1]['results'][doc] = score
+    return query_db
+
+
+def get_our_ret_list(ranking, index_path, question):
+    retriever = InformationRetrieval()
+    return retriever.get_ranking(question, ranking, index_path)
+
+
+def calc_precision_recall(our_list, ideal_list):
+    ret_rel_docs = 0
+    for rel_doc in ideal_list:
+        if rel_doc in our_list:
+            ret_rel_docs += 1
+    ret_docs = len(our_list)
+    rel_docs = len(ideal_list)
+    return (ret_rel_docs / ret_docs), (ret_rel_docs / rel_docs)
+
+
+def calc_DCG(docs, n=10):
+    docs_list = [item[1] for item in sorted(docs.items(), key=lambda x: x[1], reverse=True)]
+    dcg = int(docs_list[0])
+    i = 1
+    for doc_idx in range(i, min(n, len(docs_list))):
+        dcg += int(docs_list[doc_idx]) / (np.log2(i+1))
+    return dcg
+
+
+def test(ranking, index_path, xml_path):
+    query_db = load_query_db(xml_path)
+    for query in query_db:
+        our_list = get_our_ret_list(ranking, index_path, query['query'])
+        ideal_list = query['results']
+        precision, recall = calc_precision_recall(our_list, ideal_list)
+        print(precision, recall)
+        F = (2 * precision * recall) / (precision + recall)
+        dcg = calc_DCG(our_list)
+        idcg = calc_DCG(ideal_list)
+        ndcg = dcg / idcg
+        print(f"\nquery = {query['query']}\n\tprecision = {precision}\n\trecall = {recall}\n\tF = {F}\n\tNDCG = {ndcg}\n")
+
+
+if __name__ == '__main__':
+    path = r'cfquery.xml'
+    test(ranking='tf-idf', index_path='', xml_path=path)
